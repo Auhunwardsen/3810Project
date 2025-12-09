e0c749013ce19603e4b7fce04d886199d8677772
Austin <auhun47@iastate.edu>
2025-12-09 14:08:19 -0600

    TB looking

--- Project2/hw/cpre3810-toolflow/proj/test/tb_jalr_debug.vhd
+++ Project2/hw/cpre3810-toolflow/proj/test/tb_jalr_debug.vhd
@@ -35,30 +35,30 @@ signal reset_done : std_logic := '0';
 signal alu_out : std_logic_vector(N-1 downto 0);
 
 -- Pipeline stage signals (hierarchical access)
-alias PC is <<signal MyRiscv.s_PC : std_logic_vector(N-1 downto 0)>>;
-alias IFID_Inst is <<signal MyRiscv.s_IFID_Inst : std_logic_vector(N-1 downto 0)>>;
-alias IDEX_Inst is <<signal MyRiscv.s_IDEX_Instr : std_logic_vector(N-1 downto 0)>>;
-alias EXMEM_Inst is <<signal MyRiscv.s_EXMEM_Inst : std_logic_vector(N-1 downto 0)>>;
-alias MEMWB_Inst is <<signal MyRiscv.s_MEMWB_Inst : std_logic_vector(N-1 downto 0)>>;
+alias PC is <<signal .tb_jalr_debug.MyRiscv.s_PC : std_logic_vector(N-1 downto 0)>>;
+alias IFID_Inst is <<signal .tb_jalr_debug.MyRiscv.s_IFID_Inst : std_logic_vector(N-1 downto 0)>>;
+alias IDEX_Inst is <<signal .tb_jalr_debug.MyRiscv.s_IDEX_Instr : std_logic_vector(N-1 downto 0)>>;
+alias EXMEM_Inst is <<signal .tb_jalr_debug.MyRiscv.s_EXMEM_Inst : std_logic_vector(N-1 downto 0)>>;
+alias MEMWB_Inst is <<signal .tb_jalr_debug.MyRiscv.s_MEMWB_Inst : std_logic_vector(N-1 downto 0)>>;
 
-alias IDEX_RD is <<signal MyRiscv.s_IDEX_RDAddr : std_logic_vector(4 downto 0)>>;
-alias EXMEM_RD is <<signal MyRiscv.s_EXMEM_RDAddr : std_logic_vector(4 downto 0)>>;
-alias MEMWB_RD is <<signal MyRiscv.s_MEMWB_RDAddr : std_logic_vector(4 downto 0)>>;
+alias IDEX_RD is <<signal .tb_jalr_debug.MyRiscv.s_IDEX_RDAddr : std_logic_vector(4 downto 0)>>;
+alias EXMEM_RD is <<signal .tb_jalr_debug.MyRiscv.s_EXMEM_RDAddr : std_logic_vector(4 downto 0)>>;
+alias MEMWB_RD is <<signal .tb_jalr_debug.MyRiscv.s_MEMWB_RDAddr : std_logic_vector(4 downto 0)>>;
 
-alias IDEX_RegWrite is <<signal MyRiscv.s_IDEX_RegWrite : std_logic>>;
-alias EXMEM_RegWrite is <<signal MyRiscv.s_EXMEM_RegWrite : std_logic>>;
-alias MEMWB_RegWrite is <<signal MyRiscv.s_MEMWB_RegWrite : std_logic>>;
+alias IDEX_RegWrite is <<signal .tb_jalr_debug.MyRiscv.s_IDEX_RegWrite : std_logic>>;
+alias EXMEM_RegWrite is <<signal .tb_jalr_debug.MyRiscv.s_EXMEM_RegWrite : std_logic>>;
+alias MEMWB_RegWrite is <<signal .tb_jalr_debug.MyRiscv.s_MEMWB_RegWrite : std_logic>>;
 
-alias regWr is <<signal MyRiscv.s_RegWr : std_logic>>;
-alias regWrAddr is <<signal MyRiscv.s_RegWrAddr : std_logic_vector(4 downto 0)>>;
-alias regWrData is <<signal MyRiscv.s_RegWrData : std_logic_vector(N-1 downto 0)>>;
+alias regWr is <<signal .tb_jalr_debug.MyRiscv.s_RegWr : std_logic>>;
+alias regWrAddr is <<signal .tb_jalr_debug.MyRiscv.s_RegWrAddr : std_logic_vector(4 downto 0)>>;
+alias regWrData is <<signal .tb_jalr_debug.MyRiscv.s_RegWrData : std_logic_vector(N-1 downto 0)>>;
 
-alias s_Jump is <<signal MyRiscv.s_Jump : std_logic>>;
-alias s_ControlHazard is <<signal MyRiscv.s_ControlHazard : std_logic>>;
-alias s_IFID_Flush is <<signal MyRiscv.s_IFID_Flush : std_logic>>;
-alias s_IDEX_Flush is <<signal MyRiscv.s_IDEX_Flush : std_logic>>;
+alias s_Jump is <<signal .tb_jalr_debug.MyRiscv.s_Jump : std_logic>>;
+alias s_ControlHazard is <<signal .tb_jalr_debug.MyRiscv.s_ControlHazard : std_logic>>;
+alias s_IFID_Flush is <<signal .tb_jalr_debug.MyRiscv.s_IFID_Flush : std_logic>>;
+alias s_IDEX_Flush is <<signal .tb_jalr_debug.MyRiscv.s_IDEX_Flush : std_logic>>;
 
-alias halt is <<signal MyRiscv.s_Halt : std_logic>>;
+alias halt is <<signal .tb_jalr_debug.MyRiscv.s_Halt : std_logic>>;
 
 begin
 
@@ -68,8 +68,8 @@ begin
     iCLK      => CLK,
     iRST      => reset,
     iInstLd   => '0',
-    iInstAddr => (others => '-'),
-    iInstExt  => (others => '-'),
+    iInstAddr => (others => '0'),
+    iInstExt  => (others => '0'),
     oALUOut   => alu_out);
 
   -- Clock generation
